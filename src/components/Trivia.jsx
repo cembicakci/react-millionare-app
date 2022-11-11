2c1aa7b22f608abf83290e7591344e498da56dee
Cem <cmbicakci@gmail.com>
2022-11-11 20:41:54 +0300

    correct and wrong answer

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -13,12 +13,26 @@ function Trivia({ data, setStop, setQuestionNumber, questionNumber }) {
         setQuestion(data[questionNumber - 1])
     }, [data, questionNumber])
 
+    const delay = (duration, callback) => {
+        setTimeout(() => {
+            callback();
+        }, duration)
+    }
+
     const handleClick = (answer) => {
         setSelectedAnswer(answer)
         setClasName('active answer')
-        setTimeout(() => {
+        delay(3000, () => {
             setClasName(answer.correct ? 'answer correct' : 'answer wrong')
-        }, 3000)
+        })
+        delay(6000, () => {
+            if (answer.correct) {
+                setQuestionNumber(prev => prev + 1)
+                setSelectedAnswer('')
+            } else {
+                setStop(true)
+            }
+        })
     }
     return (
         <div className='trivia'>
