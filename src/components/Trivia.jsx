85080afc0f83a51e91cd6c2158a6944b3e3affbf
Cem <cmbicakci@gmail.com>
2022-11-11 20:24:52 +0300

    setTimeout

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
-function Trivia() {
+function Trivia({ data, setStop, setQuestionNumber, questionNumber }) {
+
+    const [question, setQuestion] = useState('');
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    const [className, setClasName] = useState('');
+
+    console.log(data)
+
+    useEffect(() => {
+        setQuestion(data[questionNumber - 1])
+    }, [data, questionNumber])
+
+    const handleClick = (answer) => {
+        setSelectedAnswer(answer)
+        setClasName('active answer')
+        setTimeout(() => {
+            setClasName(answer.correct ? 'answer correct' : 'answer wrong')
+        }, 3000)
+    }
     return (
         <div className='trivia'>
-            <div className="question">What is the best youtube channel?</div> 
+            <div className="question">{question?.question}</div>
             <div className="answers">
-                <div className="answer">Lama Dev</div>
-                <div className="answer">Lama Dev</div>
-                <div className="answer">Lama Dev</div>
-                <div className="answer">Lama Dev</div>
+                {question && question.answers.map(answer => (
+                    <div className={selectedAnswer === answer ? className : "answer"} key={answer.id} onClick={() => handleClick(answer)}>{answer.text}</div>
+                ))}
             </div>
         </div>
 
